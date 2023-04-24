20e47cb06d52674c362b88f52c54119633802af9
JEANETTE LEYVA <jeanettecleyva@outlook.com>
2023-04-24 17:57:57 -0600

    Finished with About Path

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import axios from 'axios';
+
+
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,6 +13,18 @@ class BestBooks extends React.Component {
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  componentDidMount() {
+    axios.get('/books')
+      .then(res => res.json())
+      .then(data => {
+        if (data.length > 0) {
+          this.setState({ books: data });
+        } else {
+          this.setState({ books: [] });
+        }
+      })
+      .catch(err => console.error(err));
+  }
 
   render() {
 
@@ -19,7 +35,21 @@ class BestBooks extends React.Component {
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+          <Carousel>
+            {this.state.books.map(book => (
+              <Carousel.Item key={book._id}>
+                <img
+                  className="d-block w-100"
+                  src={book.image_url}
+                  alt={book.title}
+                />
+                <Carousel.Caption>
+                  <h3>{book.title}</h3>
+                  <p>{book.description}</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            ))}
+          </Carousel>
         ) : (
           <h3>No Books Found :(</h3>
         )}
